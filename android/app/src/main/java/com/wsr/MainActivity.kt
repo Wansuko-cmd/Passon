a69054d00b61d1738bcd42d745f138fad0a38b93
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-02-13 11:02:09 +0900

    [fix] status barの色を変更

--- android/app/src/main/java/com/wsr/MainActivity.kt
+++ android/app/src/main/java/com/wsr/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.wsr
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.ColorUtils
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import java.lang.Float.max
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +22,16 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
 
         setupActionBarWithNavController(navController)
+        window.statusBarColor = getColor(R.color.moss_green).actionBarColorToStatusBarColor()
     }
 
     override fun onSupportNavigateUp() = navController.navigateUp()
+}
+
+private fun Int.actionBarColorToStatusBarColor(): Int {
+    val hsl = FloatArray(3)
+    ColorUtils.colorToHSL(this, hsl)
+    hsl[0] = max(0F, hsl[0] - 6)
+    hsl[2] = max(0F, hsl[2] - 0.09F)
+    return ColorUtils.HSLToColor(hsl)
 }
