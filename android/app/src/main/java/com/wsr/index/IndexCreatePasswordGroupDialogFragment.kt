21054516523afda27a80f7bbcbc9190a8d33a97b
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-03-03 16:48:04 +0900

    [add] LayoutCheckboxWithTextを追加

--- android/app/src/main/java/com/wsr/index/IndexCreatePasswordGroupDialogFragment.kt
+++ android/app/src/main/java/com/wsr/index/IndexCreatePasswordGroupDialogFragment.kt
@@ -14,19 +14,19 @@ class IndexCreatePasswordGroupDialogFragment(
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
-        val editText = EditText(context).apply {
-            hint = getString(R.string.index_create_password_group_dialog_hint)
-        }
+//        val editText = EditText(context).apply {
+//            hint = getString(R.string.index_create_password_group_dialog_hint)
+//        }
 
         return AlertDialog.Builder(requireActivity()).apply {
-            setTitle(R.string.index_create_password_group_dialog_title)
-            setView(editText)
-            setPositiveButton(R.string.index_create_password_group_dialog_positive_button) { _, _ ->
-                onSubmit(
-                    editText.text.toString()
-                )
-            }
-            setNegativeButton(R.string.index_create_password_group_dialog_negative_button) { _, _ -> onCancel() }
+//            setTitle(R.string.index_create_password_group_dialog_title)
+            setView(R.layout.dialog_index_create_password_group)
+//            setPositiveButton(R.string.index_create_password_group_dialog_positive_button) { _, _ ->
+//                onSubmit(
+//                    editText.text.toString()
+//                )
+//            }
+//            setNegativeButton(R.string.index_create_password_group_dialog_negative_button) { _, _ -> onCancel() }
         }.create()
     }
 }
---
+++ android/app/src/main/java/com/wsr/layout/LayoutCheckboxWithText.kt
@@ -0,0 +1,59 @@
+package com.wsr.layout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import com.wsr.R
+import com.wsr.databinding.LayoutCheckboxWithTextBinding
+
+class LayoutCheckboxWithText @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int = 0,
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val binding: LayoutCheckboxWithTextBinding
+
+    init {
+        val inflater = LayoutInflater.from(context)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.layout_checkbox_with_text,
+            this,
+            true
+        )
+    }
+
+    private fun setText(text: String) {
+        binding.layoutCheckboxWithTextText.text = text
+    }
+
+    private fun setChecked(checked: Boolean) {
+        binding.layoutCheckboxWithTextCheckbox.isChecked = checked
+    }
+
+    private fun setOnClick(onClickListener: OnClickListener) {
+        binding.layoutCheckboxWithTextCheckbox.setOnClickListener(onClickListener)
+        binding.layoutCheckboxWithTextText.setOnClickListener(onClickListener)
+    }
+
+
+    companion object {
+
+        @BindingAdapter("text")
+        @JvmStatic
+        fun setText(view: LayoutCheckboxWithText, text: String) = view.setText(text)
+
+        @BindingAdapter("checked")
+        @JvmStatic
+        fun setChecked(view: LayoutCheckboxWithText, checked: Boolean) = view.setChecked(checked)
+
+        @BindingAdapter("onClick")
+        @JvmStatic
+        fun setOnClick(view: LayoutCheckboxWithText, onClickListener: OnClickListener) =
+            view.setOnClick(onClickListener)
+    }
+}
