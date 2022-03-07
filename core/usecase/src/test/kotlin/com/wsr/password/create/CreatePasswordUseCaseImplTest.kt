8842cad8fb5cf6024e0e5f0634b124592ec6a673
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-03-07 22:11:14 +0900

    [fix] UUIDのモックの仕方を修正

--- core/usecase/src/test/kotlin/com/wsr/password/create/CreatePasswordUseCaseImplTest.kt
+++ core/usecase/src/test/kotlin/com/wsr/password/create/CreatePasswordUseCaseImplTest.kt
@@ -3,8 +3,13 @@
 package com.wsr.password.create
 
 import com.wsr.password.PasswordUseCaseModel
+import org.mockito.MockedStatic
 import org.mockito.Mockito
+import org.mockito.kotlin.any
+import org.mockito.kotlin.given
 import java.util.*
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,19 +18,18 @@ class CreatePasswordUseCaseImplTest {
 
     private val uuid: UUID = UUID.fromString("5af48f3b-468b-4ae0-a065-7d7ac70b37a8")
 
-    init {
-        val uuidMock = Mockito.mockStatic(UUID::class.java)
-
-        uuidMock.`when`<UUID>(UUID::randomUUID).thenReturn(uuid)
-    }
-
     @Test
     fun createInstance関数にpasswordGroupIdを渡すことでPasswordUseCaseModelを新規作成() {
+        val uuidMock = Mockito.mockStatic(UUID::class.java)
+        uuidMock.`when`<UUID>(UUID::randomUUID).thenReturn(uuid)
+        given(UUID.fromString(any())).willCallRealMethod()
 
         val passwordGroupId = "PasswordGroupId"
         assertEquals(
             expected = PasswordUseCaseModel(uuid.toString(), passwordGroupId, "", ""),
             actual = createPasswordUseCase.createInstance(passwordGroupId)
         )
+
+        uuidMock.close()
     }
 }
--- core/usecase/src/test/kotlin/com/wsr/passwordgroup/create/CreatePasswordGroupUseCaseImplTest.kt
+++ core/usecase/src/test/kotlin/com/wsr/passwordgroup/create/CreatePasswordGroupUseCaseImplTest.kt
@@ -9,10 +9,15 @@ import com.wsr.state.State
 import com.wsr.user.Email
 import com.wsr.utils.UniqueId
 import kotlinx.coroutines.runBlocking
+import org.mockito.MockedStatic
 import org.mockito.Mockito
+import org.mockito.kotlin.any
 import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.given
 import org.mockito.kotlin.mock
 import java.util.*
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -20,13 +25,7 @@ class CreatePasswordGroupUseCaseImplTest {
 
     private val uuid: UUID = UUID.fromString("5af48f3b-468b-4ae0-a065-7d7ac70b37a8")
 
-    init {
-        val uuidMock = Mockito.mockStatic(UUID::class.java)
-
-        uuidMock.`when`<UUID>(UUID::randomUUID).thenReturn(uuid)
-    }
-
-    private val testPasswordGroup = PasswordGroup(UniqueId(), Email("example@gmail.com"), "", "")
+    private val testPasswordGroup = PasswordGroup(UniqueId(uuid.toString()), Email("example@gmail.com"), "", "")
 
     private val passwordGroupRepository: PasswordGroupRepository = mock {
         onBlocking { create(testPasswordGroup) } doReturn Unit
@@ -36,7 +35,16 @@ class CreatePasswordGroupUseCaseImplTest {
 
     @Test
     fun createでPasswordGroupを新規登録(): Unit = runBlocking {
+        val uuidMock = Mockito.mockStatic(UUID::class.java)
+        uuidMock.`when`<UUID>(UUID::randomUUID).thenReturn(uuid)
+        given(UUID.fromString(any())).willCallRealMethod()
+
         val newPasswordGroup = createPasswordGroupUseCaseImpl.create(testPasswordGroup.email.value, testPasswordGroup.title)
-        assertEquals(State.Success(testPasswordGroup.toUseCaseModel()), newPasswordGroup)
+        assertEquals(
+            expected = State.Success(testPasswordGroup.toUseCaseModel()),
+            actual = newPasswordGroup,
+        )
+
+        uuidMock.close()
     }
 }
