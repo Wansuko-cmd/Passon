b496f4f15e7d508702c221770d5953539b8c5295
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-03-31 18:01:06 +0900

    [fix] LoginPasswordにハッシュ前とハッシュ後の状態を追加

--- core/domain/src/main/kotlin/com/wsr/user/UserFactory.kt
+++ core/domain/src/main/kotlin/com/wsr/user/UserFactory.kt
@@ -3,6 +3,6 @@ package com.wsr.user
 class UserFactory {
     fun create(
         email: Email,
-        loginPassword: LoginPassword,
-    ): User = User(email, loginPassword)
+        loginPassword: LoginPassword.PlainLoginPassword,
+    ): User = User(email, loginPassword.toHashed())
 }
