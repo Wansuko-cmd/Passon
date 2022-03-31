0f0c7a70dcb963ea3c4690ab06bcee87d329c388
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-03-31 22:56:02 +0900

    [fix] AuthのUseCaseExceptionにSystemErrorを追加

--- core/usecase/src/main/kotlin/com/wsr/auth/LoginUseCaseException.kt
+++ core/usecase/src/main/kotlin/com/wsr/auth/LoginUseCaseException.kt
@@ -1,6 +1,12 @@
 package com.wsr.auth
 
+import com.wsr.create.CreatePasswordGroupUseCaseException
+
 sealed class LoginUseCaseException : Throwable() {
     class AuthenticationFailedException(override val message: String) : LoginUseCaseException()
     class NoSuchUserException(override val message: String) : LoginUseCaseException()
+    class SystemError(
+        override val message: String,
+        override val cause: Throwable,
+    ) : LoginUseCaseException()
 }
--- core/usecase/src/main/kotlin/com/wsr/auth/ResetUseCaseException.kt
+++ core/usecase/src/main/kotlin/com/wsr/auth/ResetUseCaseException.kt
@@ -1,6 +1,12 @@
 package com.wsr.auth
 
+import com.wsr.create.CreatePasswordGroupUseCaseException
+
 sealed class ResetUseCaseException : Throwable() {
     class AuthenticationFailedException(override val message: String) : ResetUseCaseException()
     class NoSuchUserException(override val message: String) : ResetUseCaseException()
+    class SystemError(
+        override val message: String,
+        override val cause: Throwable,
+    ) : ResetUseCaseException()
 }
