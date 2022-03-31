07ff7dfb7d6d9f82895d49946b618e4301dbbbf9
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-03-31 23:09:22 +0900

    [fix] CreatePasswordGroupUseCaseImplTestを修正

--- core/usecase/src/main/kotlin/com/wsr/fetch/FetchPasswordPairUseCase.kt
+++ core/usecase/src/main/kotlin/com/wsr/fetch/FetchPasswordPairUseCase.kt
@@ -1,11 +1,10 @@
 package com.wsr.fetch
 
 import com.wsr.PasswordPairUseCaseModel
-import com.wsr.exceptions.GetAllDataFailedException
 import com.wsr.state.State
 import kotlinx.coroutines.flow.Flow
 
 interface FetchPasswordPairUseCase {
-    val data: Flow<State<PasswordPairUseCaseModel, GetAllDataFailedException>>
+    val data: Flow<State<PasswordPairUseCaseModel, FetchPasswordPairUseCaseException>>
     suspend fun fetch(passwordGroupId: String)
 }
