a86e9fc137e163c3dcd542758a0915d9c3d3373a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-01 14:27:04 +0900

    [fix] androidのPasswordItemRepositoryを修正

--- android/app/src/main/java/com/wsr/infra/passworditem/LocalPasswordItemRepositoryImpl.kt
+++ android/app/src/main/java/com/wsr/infra/passworditem/LocalPasswordItemRepositoryImpl.kt
@@ -1,30 +1,34 @@
 package com.wsr.infra.passworditem
 
 import com.wsr.exceptions.DeleteDataFailedException
-import com.wsr.exceptions.UpdateDataFailedException
+import com.wsr.exceptions.UpsertDataFailedException
 import com.wsr.passwordgroup.PasswordGroupId
 import com.wsr.passworditem.PasswordItem
 import com.wsr.passworditem.PasswordItemId
 import com.wsr.passworditem.PasswordItemRepository
+import com.wsr.state.State
 
 class LocalPasswordItemRepositoryImpl(private val passwordEntityDao: PasswordItemEntityDao) :
     PasswordItemRepository {
 
     override suspend fun upsert(passwordItem: PasswordItem) = try {
         passwordEntityDao.upsert(passwordItem.toEntity())
+        State.Success(Unit)
     } catch (e: Exception) {
-        throw UpdateDataFailedException.DatabaseException(e.message ?: "")
+        State.Failure(UpsertDataFailedException.DatabaseException(e.message ?: ""))
     }
 
     override suspend fun delete(id: PasswordItemId) = try {
         passwordEntityDao.delete(id.value)
+        State.Success(Unit)
     } catch (e: Exception) {
-        throw DeleteDataFailedException.DatabaseException(e.message ?: "")
+        State.Failure(DeleteDataFailedException.DatabaseException(e.message ?: ""))
     }
 
     override suspend fun deleteAll(passwordGroupId: PasswordGroupId) = try {
         passwordEntityDao.deleteAllByPasswordGroupId(passwordGroupId.value)
+        State.Success(Unit)
     } catch (e: Exception) {
-        throw DeleteDataFailedException.DatabaseException(e.message ?: "")
+        State.Failure(DeleteDataFailedException.DatabaseException(e.message ?: ""))
     }
 }
