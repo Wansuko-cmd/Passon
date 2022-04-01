b9e7e2439221b94bbd58acfa545c6376097340df
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-01 13:59:10 +0900

    [fix] UserRepositoryの返り値をResult型に修正

--- core/domain/src/main/kotlin/com/wsr/user/UserRepository.kt
+++ core/domain/src/main/kotlin/com/wsr/user/UserRepository.kt
@@ -1,6 +1,10 @@
 package com.wsr.user
 
+import com.wsr.exceptions.CreateDataFailedException
+import com.wsr.exceptions.UpdateDataFailedException
+import com.wsr.state.State
+
 interface UserRepository {
-    suspend fun create(user: User)
-    suspend fun update(user: User)
+    suspend fun create(user: User): State<Unit, CreateDataFailedException>
+    suspend fun update(user: User): State<Unit, UpdateDataFailedException>
 }
