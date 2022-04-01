7fefb6df00e00d770fcdac3a592e39b574f2b7f5
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-01 13:57:10 +0900

    [fix] PasswordGroupRepositoryの返り値をResult型に修正

--- core/domain/src/main/kotlin/com/wsr/passwordgroup/PasswordGroupRepository.kt
+++ core/domain/src/main/kotlin/com/wsr/passwordgroup/PasswordGroupRepository.kt
@@ -3,14 +3,12 @@ package com.wsr.passwordgroup
 import com.wsr.exceptions.CreateDataFailedException
 import com.wsr.exceptions.DeleteDataFailedException
 import com.wsr.exceptions.UpdateDataFailedException
+import com.wsr.state.State
 
 interface PasswordGroupRepository {
-    @Throws(CreateDataFailedException::class)
-    suspend fun create(passwordGroup: PasswordGroup)
+    suspend fun create(passwordGroup: PasswordGroup): State<Unit, CreateDataFailedException>
 
-    @Throws(UpdateDataFailedException::class)
-    suspend fun update(id: PasswordGroupId, title: String, remark: String)
+    suspend fun update(id: PasswordGroupId, title: String, remark: String): State<Unit, UpdateDataFailedException>
 
-    @Throws(DeleteDataFailedException::class)
-    suspend fun delete(id: PasswordGroupId)
+    suspend fun delete(id: PasswordGroupId): State<Unit, DeleteDataFailedException>
 }
