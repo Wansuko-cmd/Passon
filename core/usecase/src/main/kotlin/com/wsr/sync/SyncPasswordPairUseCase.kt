37e7b4ebf5d78d50254789da6e960dc503bbdf1b
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-02 10:18:25 +0900

    [fix] syncのファイルを整理

--- core/usecase/src/main/kotlin/com/wsr/sync/SyncPasswordPairUseCase.kt
+++ core/usecase/src/main/kotlin/com/wsr/sync/SyncPasswordPairUseCase.kt
@@ -11,3 +11,11 @@ interface SyncPasswordPairUseCase {
         passwordItems: List<PasswordItemUseCaseModel>,
     ): Maybe<Unit, SyncPasswordPairUseCaseException>
 }
+
+sealed class SyncPasswordPairUseCaseException : Throwable() {
+    data class NoSuchPasswordGroupException(override val message: String) : SyncPasswordPairUseCaseException()
+    data class SystemError(
+        override val message: String,
+        override val cause: Throwable,
+    ) : SyncPasswordPairUseCaseException()
+}
--- core/usecase/src/main/kotlin/com/wsr/sync/SyncPasswordPairUseCaseException.kt
+++
@@ -1,9 +0,0 @@
-package com.wsr.sync
-
-sealed class SyncPasswordPairUseCaseException : Throwable() {
-    data class NoSuchPasswordGroupException(override val message: String) : SyncPasswordPairUseCaseException()
-    data class SystemError(
-        override val message: String,
-        override val cause: Throwable,
-    ) : SyncPasswordPairUseCaseException()
-}
