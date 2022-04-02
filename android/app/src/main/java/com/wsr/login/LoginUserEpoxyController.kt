5f872bc99df069773e4b9189bc84617388cc76e9
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-02 17:47:56 +0900

    [add] LoginUserEpoxyControllerへの記述を追加

--- android/app/src/main/java/com/wsr/login/LoginUserEpoxyController.kt
+++ android/app/src/main/java/com/wsr/login/LoginUserEpoxyController.kt
@@ -1,6 +1,18 @@
 package com.wsr.login
 
+import android.view.View
 import com.airbnb.epoxy.TypedEpoxyController
+import com.wsr.loginUserRow
 
-class LoginUserEpoxyController : TypedEpoxyController {
+class LoginUserEpoxyController(private val onSelected: (userId: String) -> Unit) : TypedEpoxyController<List<UserLoginUiState>>() {
+    override fun buildModels(users: List<UserLoginUiState>) {
+        users.forEach { user ->
+            loginUserRow {
+                displayName(user.displayName)
+                databasePath(user.databasePath)
+                isSelected(user.isSelected)
+                onSelected(View.OnClickListener { this@LoginUserEpoxyController.onSelected(user.id) })
+            }
+        }
+    }
 }
