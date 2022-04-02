a99a93bb3660b74af14a3f0e11f2f6b6c4d63a83
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-02 16:15:18 +0900

    [add] SignUpFragmentの内容を追加

--- android/app/src/main/java/com/wsr/signup/SignUpFragment.kt
+++ android/app/src/main/java/com/wsr/signup/SignUpFragment.kt
@@ -1,6 +1,38 @@
 package com.wsr.signup
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
 import com.wsr.R
+import com.wsr.databinding.FragmentSignUpBinding
+import com.wsr.ext.launchInLifecycleScope
+import com.wsr.layout.AfterTextChanged
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SignUpFragment : Fragment(R.layout.fragment_sign_up)
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+    private val signUpViewModel by viewModel<SignUpViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+
+        FragmentSignUpBinding.bind(view).apply {
+            afterDisplayNameChanged = AfterTextChanged(signUpViewModel::updateDisplayName)
+            afterLoginPasswordChanged = AfterTextChanged(signUpViewModel::updateLoginPassword)
+            signUpFragmentNextButton.setOnClickListener { signUpViewModel.signUp() }
+        }
+
+        launchInLifecycleScope(Lifecycle.State.STARTED) {
+            signUpViewModel.navigateToIndexEvent.collect { navigateToIndex() }
+        }
+    }
+
+    private fun navigateToIndex() {
+        val action = SignUpFragmentDirections.actionSignUpFragmentToIndexFragment()
+        findNavController().navigate(action)
+    }
+}
--- android/app/src/main/java/com/wsr/signup/SignUpViewModel.kt
+++ android/app/src/main/java/com/wsr/signup/SignUpViewModel.kt
@@ -3,13 +3,19 @@ package com.wsr.signup
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wsr.auth.SignUpUseCase
+import com.wsr.maybe.consume
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 class SignUpViewModel(private val signUpUseCase: SignUpUseCase) : ViewModel() {
 
     private val _displayName = MutableStateFlow("")
     private val _loginPassword = MutableStateFlow("")
+
+    private val _navigateToIndexEvent = MutableSharedFlow<Unit>(replay = 0)
+    val navigateToIndexEvent get() = _navigateToIndexEvent.asSharedFlow()
 
     fun updateDisplayName(displayName: String) {
         viewModelScope.launch { _displayName.emit(displayName) }
@@ -21,7 +27,11 @@ class SignUpViewModel(private val signUpUseCase: SignUpUseCase) : ViewModel() {
 
     fun signUp() {
         viewModelScope.launch {
-            signUpUseCase.signUp(_displayName.value, "Local", _loginPassword.value)
+            signUpUseCase.signUp(
+                displayName = _displayName.value,
+                databasePath = "Local",
+                loginPassword = _loginPassword.value,
+            ).consume { _navigateToIndexEvent.emit(Unit) }
         }
     }
 }
