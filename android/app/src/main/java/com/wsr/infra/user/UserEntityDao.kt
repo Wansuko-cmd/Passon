4aec76c9b0c8c4f6b7266a60be222c2bbef5a8d3
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-02 10:38:00 +0900

    [add] UserEntityDaoを追加

---
+++ android/app/src/main/java/com/wsr/infra/user/UserEntityDao.kt
@@ -0,0 +1,18 @@
+package com.wsr.infra.user
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+interface UserEntityDao {
+
+    @Query("SELECT * FROM user WHERE email = :email")
+    suspend fun getByEmail(email: String): UserEntity
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insert(userEntity: UserEntity)
+
+    @Update
+    suspend fun update(userEntity: UserEntity)
+}
