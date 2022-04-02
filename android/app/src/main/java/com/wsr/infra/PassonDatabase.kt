5de08f0f6b95f744a0d30f94e0e7c5338c456687
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-02 10:55:55 +0900

    [edit] PassonDatabaseに追記

--- android/app/src/main/java/com/wsr/infra/PassonDatabase.kt
+++ android/app/src/main/java/com/wsr/infra/PassonDatabase.kt
@@ -6,9 +6,12 @@ import com.wsr.infra.passwordgroup.PasswordGroupEntity
 import com.wsr.infra.passwordgroup.PasswordGroupEntityDao
 import com.wsr.infra.passworditem.PasswordItemEntity
 import com.wsr.infra.passworditem.PasswordItemEntityDao
+import com.wsr.infra.user.UserEntity
+import com.wsr.infra.user.UserEntityDao
 
-@Database(entities = [PasswordItemEntity::class, PasswordGroupEntity::class], version = 1)
+@Database(entities = [PasswordItemEntity::class, PasswordGroupEntity::class, UserEntity::class], version = 1)
 abstract class PassonDatabase : RoomDatabase() {
     abstract fun passwordGroupEntityDao(): PasswordGroupEntityDao
     abstract fun passwordItemEntityDao(): PasswordItemEntityDao
+    abstract fun userEntityDao(): UserEntityDao
 }
