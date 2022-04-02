a92b3833f3e5c59eecc35585ccdb6de45394a770
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-02 11:00:39 +0900

    [fix] UserEntityを追加

--- android/app/src/main/java/com/wsr/infra/user/UserEntity.kt
+++ android/app/src/main/java/com/wsr/infra/user/UserEntity.kt
@@ -3,23 +3,27 @@ package com.wsr.infra.user
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.wsr.user.UserId
+import com.wsr.user.DatabasePath
 import com.wsr.user.LoginPassword
 import com.wsr.user.User
+import com.wsr.user.UserId
 
 @Entity(tableName = "user")
 data class UserEntity(
     @PrimaryKey val email: String,
+    @ColumnInfo(name = "database_path") val databasePath: String,
     @ColumnInfo(name = "login_password") val loginPassword: String,
 ) {
 
     fun toUser() = User(
         userId = UserId(email),
+        databasePath = DatabasePath(databasePath),
         loginPassword = LoginPassword.HashedLoginPassword(loginPassword)
     )
 }
 
 fun User.toEntity() = UserEntity(
     email = userId.value,
+    databasePath = databasePath.value,
     loginPassword = loginPassword.value,
 )
