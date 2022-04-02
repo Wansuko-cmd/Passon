e5e5a2358159eb808b2381b532f6d0a95186df26
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-02 18:05:53 +0900

    [fix] QueryServiceの返り値を変更

--- core/usecase/src/main/kotlin/com/wsr/get/GetAllPasswordGroupUseCaseImpl.kt
+++ core/usecase/src/main/kotlin/com/wsr/get/GetAllPasswordGroupUseCaseImpl.kt
@@ -1,8 +1,10 @@
 package com.wsr.get
 
+import com.wsr.maybe.mapBoth
 import com.wsr.maybe.mapFailure
 import com.wsr.queryservice.PasswordGroupQueryService
 import com.wsr.queryservice.PasswordGroupQueryServiceException
+import com.wsr.toUseCaseModel
 import com.wsr.user.UserId
 
 class GetAllPasswordGroupUseCaseImpl(
@@ -12,7 +14,10 @@ class GetAllPasswordGroupUseCaseImpl(
     override suspend fun get(userId: String) =
         passwordGroupQueryService
             .getAll(UserId(userId))
-            .mapFailure { it.toGetAllPasswordGroupUseCaseException() }
+            .mapBoth(
+                success = { users -> users.map { it.toUseCaseModel() } },
+                failure = { it.toGetAllPasswordGroupUseCaseException() },
+            )
 
     private fun PasswordGroupQueryServiceException.toGetAllPasswordGroupUseCaseException() = when (this) {
         is PasswordGroupQueryServiceException.DatabaseError ->
--- core/usecase/src/main/kotlin/com/wsr/queryservice/PasswordGroupQueryService.kt
+++ core/usecase/src/main/kotlin/com/wsr/queryservice/PasswordGroupQueryService.kt
@@ -2,10 +2,11 @@ package com.wsr.queryservice
 
 import com.wsr.PasswordGroupUseCaseModel
 import com.wsr.maybe.Maybe
+import com.wsr.passwordgroup.PasswordGroup
 import com.wsr.user.UserId
 
 interface PasswordGroupQueryService {
-    suspend fun getAll(userId: UserId): Maybe<List<PasswordGroupUseCaseModel>, PasswordGroupQueryServiceException>
+    suspend fun getAll(userId: UserId): Maybe<List<PasswordGroup>, PasswordGroupQueryServiceException>
 }
 
 sealed class PasswordGroupQueryServiceException : Throwable() {
