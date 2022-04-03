4c2556cb2a8bcf2bf6ef748baf70cb301c7ac893
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-03 22:50:06 +0900

    [add] UpdateUserUseCaseImplを実装

--- core/usecase/src/main/kotlin/com/wsr/update/UpdateUserUseCaseImpl.kt
+++ core/usecase/src/main/kotlin/com/wsr/update/UpdateUserUseCaseImpl.kt
@@ -1,4 +1,28 @@
 package com.wsr.update
 
-class UpdateUserUseCaseImpl {
+import com.wsr.exceptions.UpdateDataFailedException
+import com.wsr.maybe.Maybe
+import com.wsr.maybe.mapFailure
+import com.wsr.user.DisplayName
+import com.wsr.user.UserId
+import com.wsr.user.UserRepository
+
+class UpdateUserUseCaseImpl(private val userRepository: UserRepository) : UpdateUserUseCase {
+    override suspend fun update(
+        userId: String,
+        displayName: String,
+    ): Maybe<Unit, UpdateUserUseCaseException> =
+        userRepository
+            .update(UserId(userId), DisplayName(displayName))
+            .mapFailure { it.toUpdateUserUseCaseException() }
+
+    private fun UpdateDataFailedException.toUpdateUserUseCaseException() = when(this) {
+        is UpdateDataFailedException.NoSuchElementException ->
+            UpdateUserUseCaseException.NoSuchUserException(this.message)
+        is UpdateDataFailedException.DatabaseError ->
+            UpdateUserUseCaseException.SystemError(
+                message = this.message,
+                cause = this,
+            )
+    }
 }
