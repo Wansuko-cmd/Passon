02ff799e591c9ac12136fd3f7e44f534f4426f0c
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-03 22:56:47 +0900

    [fix] AndroidのUserRepositoryを修正

--- android/app/src/main/java/com/wsr/infra/user/LocalUserRepositoryImpl.kt
+++ android/app/src/main/java/com/wsr/infra/user/LocalUserRepositoryImpl.kt
@@ -3,7 +3,10 @@ package com.wsr.infra.user
 import com.wsr.exceptions.CreateDataFailedException
 import com.wsr.exceptions.UpdateDataFailedException
 import com.wsr.maybe.Maybe
+import com.wsr.user.DisplayName
+import com.wsr.user.LoginPassword
 import com.wsr.user.User
+import com.wsr.user.UserId
 import com.wsr.user.UserRepository
 
 class LocalUserRepositoryImpl(private val userEntityDao: UserEntityDao) : UserRepository {
@@ -14,8 +17,25 @@ class LocalUserRepositoryImpl(private val userEntityDao: UserEntityDao) : UserRe
         Maybe.Failure(CreateDataFailedException.DatabaseError(e.message.orEmpty()))
     }
 
-    override suspend fun update(user: User): Maybe<Unit, UpdateDataFailedException> = try {
-        userEntityDao.update(user.toEntity())
+    override suspend fun update(
+        userId: UserId,
+        displayName: DisplayName,
+    ): Maybe<Unit, UpdateDataFailedException> = try {
+        userEntityDao.getById(userId.value)
+            .copy(displayName = displayName.value)
+            .also { userEntityDao.update(it) }
+        Maybe.Success(Unit)
+    } catch (e: Exception) {
+        Maybe.Failure(UpdateDataFailedException.DatabaseError(e.message.orEmpty()))
+    }
+
+    override suspend fun update(
+        userId: UserId,
+        loginPassword: LoginPassword.HashedLoginPassword,
+    ): Maybe<Unit, UpdateDataFailedException> = try {
+        userEntityDao.getById(userId.value)
+            .copy(loginPassword = loginPassword.value)
+            .also { userEntityDao.update(it) }
         Maybe.Success(Unit)
     } catch (e: Exception) {
         Maybe.Failure(UpdateDataFailedException.DatabaseError(e.message.orEmpty()))
