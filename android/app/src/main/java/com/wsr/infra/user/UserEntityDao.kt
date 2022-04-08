ad6695bdb61528d084e03f5a8d66206631f7f2f0
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-08 23:55:26 +0900

    [fix] UserRepositoryImplを修正

--- android/app/src/main/java/com/wsr/infra/user/UserEntityDao.kt
+++ android/app/src/main/java/com/wsr/infra/user/UserEntityDao.kt
@@ -1,6 +1,7 @@
 package com.wsr.infra.user
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -20,4 +21,7 @@ interface UserEntityDao {
 
     @Update
     suspend fun update(userEntity: UserEntity)
+
+    @Query("DELETE FROM users WHERE id=:id")
+    suspend fun delete(id: String)
 }
