4024704fd53faa49e24142d70b9d3854a8c2a582
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-04-19 08:35:16 +0900

    [add] dialogライブラリ用フォルダを追加

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,5 +9,5 @@
 
 rootProject.name = "Passon"
 include("core", "core:domain", "core:usecase", "core:repository")
-include("android", "android:app")
+include("android", "android:app", "android:dialog")
 include("utils")
